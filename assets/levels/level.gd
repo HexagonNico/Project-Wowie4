250ae7b4a4b88c82b43fac281c76b539eddf4ede
Nico <nico.hex6@gmail.com>
2022-08-13 17:15:13 +0200

    Better camera movement

--- assets/levels/level.gd
+++ assets/levels/level.gd
@@ -3,7 +3,21 @@ extends Node2D
 
 export var next_level: PackedScene
 
+onready var robot = $Robot
+onready var bubble_wrap_ball = $BubbleWrapBall
+onready var camera = $Camera
 onready var animation_player = $AnimationPlayer
+
+
+func _process(_delta: float):
+	var robot_valid = is_instance_valid(robot)
+	var bubble_wrap_ball_valid = is_instance_valid(bubble_wrap_ball)
+	if robot_valid and bubble_wrap_ball_valid:
+		camera.global_position = (robot.global_position + bubble_wrap_ball.global_position) / 2
+	elif robot_valid:
+		camera.global_position = robot.global_position
+	elif bubble_wrap_ball_valid:
+		camera.global_position = bubble_wrap_ball.global_position
 
 
 func reset_level():
