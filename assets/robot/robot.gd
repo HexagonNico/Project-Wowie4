5bf6660eb98bd57c248a2c4ab004c2faaba7da24
Nico <nico.hex6@gmail.com>
2022-08-13 15:02:49 +0200

    Bubble wrap ball in level 1

--- assets/robot/robot.gd
+++ assets/robot/robot.gd
@@ -62,7 +62,7 @@ func kick():
 	if kick_ray.is_colliding():
 		var collider = kick_ray.get_collider()
 		if collider is RigidBody2D:
-			collider.linear_velocity = Vector2(100.0, -250.0) * sprite.scale
+			collider.linear_velocity = Vector2(150.0, -200.0) * sprite.scale
 
 
 func set_jump(value: bool):
