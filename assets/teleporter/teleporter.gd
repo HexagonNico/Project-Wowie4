4fd6c6bfc085e39e675816e742060c4dc3a61160
Nico <nico.hex6@gmail.com>
2022-08-14 15:10:40 +0200

    Teleport particles

--- assets/teleporter/teleporter.gd
+++ assets/teleporter/teleporter.gd
@@ -4,13 +4,23 @@ extends Node2D
 signal teleported
 
 
+export var teleport_particles: PackedScene
+
 var ball_entered: bool = false
 
 
 func _on_body_entered(body: Node2D):
 	if not ball_entered and body is RigidBody2D:
+		var particles: Particles2D = teleport_particles.instance()
+		particles.emitting = true
+		get_parent().add_child(particles)
+		particles.position = body.position
 		body.queue_free()
 		ball_entered = true
 	elif ball_entered and body is KinematicBody2D:
+		var particles: Particles2D = teleport_particles.instance()
+		particles.emitting = true
+		get_parent().add_child(particles)
+		particles.position = body.position
 		body.queue_free()
 		emit_signal("teleported")
